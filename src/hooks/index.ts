61aa3c7370c4790c8c3d8e4d998e443e58ee6b31
Serkan Aydin <serayd61@hotmail.com>
2025-12-30 16:14:14 +0100

    feat: Add hooks index exports

---
+++ src/hooks/index.ts
@@ -0,0 +1,12 @@
+// Hook exports for DeFi Sentinel
+
+export { useLocalStorage } from './useLocalStorage';
+export { useDebounce, useDebouncedCallback } from './useDebounce';
+export { 
+  useMediaQuery, 
+  useIsMobile, 
+  useIsTablet, 
+  useIsDesktop,
+  usePrefersDarkMode,
+  usePrefersReducedMotion 
+} from './useMediaQuery';
